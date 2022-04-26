0c2bf7f171e29a347721b1d9b934c76d2e4e1d9b
Aleksander <90185121+AKopydlowski@users.noreply.github.com>
2022-04-26 15:43:08 +0200

    Lekcja 17_2

--- Pracownia_Programowania_Obiektowego/Lekcja 17_2/pliki.cpp
+++ Pracownia_Programowania_Obiektowego/Lekcja 17_2/pliki.cpp
@@ -1,25 +1,42 @@
+//Â© 2022 Aleksander K.  All rights reserved.
 #include <iostream>
 #include <fstream>
+#include <stdlib.h>
 using namespace std;
 
 int main(){
 	setlocale(LC_ALL, "polish");
 	
 	ofstream File;
-	File.open("C:\\Users\\student\\Desktop\\Lekcja 17_2\\pliki\\plik.txt");
+	File.open("C:\\Users\\student\\Desktop\\17_2\\text.txt", ios::app);
 	
 	if(File.is_open()){
-		cout<<("Podaj dane do zapisu w pliku: ", ios::app)<<endl;
+		cout<<("Enter the data to be written to the file: ");
 		string text;
-		//cin nie ³apie spacji
+		//cin nie lapie spacji
 //		cin>>text;
-		//³apie spacje
+		//lapie spacje
 		getline(cin, text);
 		//przekierowane dane z text do pliku
 		File<<text<<endl;
 		File.close();
 	}else{
 		cout<<"ERROR 404"<<endl;
+	}
+	
+	system("cls");
+	 
+	ifstream FileOpen("C:\\Users\\student\\Desktop\\17_2\\text.txt");
+	if(FileOpen.is_open()){
+		cout<<"File content"<<endl;
+		string line;
+		//wyswietlanie wszystkich lini z pliku
+		while(getline(FileOpen, line)){
+			cout<<line<<endl;
+		}
+		FileOpen.close();	
+	}else{
+		cout<<"ERROR 321"<<endl;	
 	}
 	
 	return 0;
